81408d5ac6388f806823cdd28ba26288c3aa44b5
alguiguilo098 <guiguigui098@gmail.com>
2026-06-24 15:27:20 -0300

    feat: processDummy Processes

--- processDummy.py
+++ processDummy.py
@@ -11,11 +11,14 @@ logging.basicConfig(
 )
 
 KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
-KAFKA_TOPIC = 'netflow-topic'
+KAFKA_TOPIC = 'monitoring-topic'
 ELASTIC_HOSTS = ['https://localhost:9200']
 ELASTIC_USER = 'elastic'
 ELASTIC_PASSWORD = 'LM5v1tZ0wLT7WvULS4DB'
-ELASTIC_INDEX = 'netflow-logs'
+ELASTIC_INDEX = 'monitoring-logs'
+ELASTIC_INDEX1= 'url-logs'
+ELASTIC_INDEX2= 'programs-logs'
+ELASTIC_INDEX3= 'kernel-logs'
 
 def connect_elasticsearch():
     return Elasticsearch(
@@ -29,7 +32,7 @@ def create_kafka_consumer():
         KAFKA_TOPIC,
         bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
         auto_offset_reset='latest',  
-        group_id='netflow-consumer-group',
+        group_id='monitoring-consumer-group',
         enable_auto_commit=True,      
         value_deserializer=lambda x: json.loads(x.decode('utf-8'))
     )
@@ -44,14 +47,25 @@ def main():
             
             for message in consumer:
                 try:
-                    processed_data = message.value
-                    processed_data['@timestamp'] = datetime.utcnow().isoformat()
-
-                    es.index(
-                        index=ELASTIC_INDEX,
-                        document=processed_data
-                    )
-                    logging.info(f"Dados Enviado: {processed_data}")
+                    if "processes" in message.value:
+                        for process in message.value["processes"]:
+                            logging.info(f"Mensagem recebida: {process}")
+                            es.index(index=ELASTIC_INDEX, document=process)
+                    elif "url" in message.value:
+                        logging.info(f"Mensagem recebida: {message.value['url']}")
+                        es.index(index=ELASTIC_INDEX1, document=message.value)
+                        logging.info("Mensagem enviada ao Elasticsearch com sucesso.")
+                    elif "programs" in message.value:
+                        logging.info(f"Mensagem recebida: {message.value['programs']}")
+                        es.index(index=ELASTIC_INDEX2, document=message.value)
+                        logging.info("Mensagem enviada ao Elasticsearch com sucesso.")
+                    elif "kernel" in message.value:
+                        logging.info(f"Mensagem recebida: {message.value['kernel']}")
+                        es.index(index=ELASTIC_INDEX3, document=message.value)
+                        logging.info("Mensagem enviada ao Elasticsearch com sucesso.")
+                    else:
+                        logging.info(f"Mensagem recebida com formato inesperado: {message.value}")
+                        es.index(index=ELASTIC_INDEX, document=message.value)
                 except Exception as e:
                     logging.error(f"Erro ao enviar mensagem ao Elasticsearch: {e}")
 
